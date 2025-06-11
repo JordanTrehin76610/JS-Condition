883034e02637bca6c585061388731cba142e927b
JordanTrehin76610 <jordantrehinpro@gmail.com>
2025-06-11 10:20:45 +0200

    Maj typage

--- exo1/assets/js/script.js
+++ exo1/assets/js/script.js
@@ -1,11 +1,7 @@
 let age = +prompt("Rentrez votre âge")
 
 if (age >= 18) {
-
     console.log("l'utilisateur est majeur")
-
 } else {
-
     console.log("l'utilisateur est mineur")
-
 }
--- exo2/assets/js/script.js
+++ exo2/assets/js/script.js
@@ -1,16 +1,10 @@
 let entier = +prompt("Rentrez un nombre entier")
 
 if (isNaN(entier)) {
-
     console.log("L'utilisateur à rentrer une information non valide")
-
 } else if (entier % 2 === 0) {
-
     console.log("Ce nombre est pair")
-
 }
 else {
-
     console.log("Ce nombre est impair")
-
 }
